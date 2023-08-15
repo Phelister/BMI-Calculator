0d193bcb50c47622ce971088a47f67bd0d5953e9
pbogonko <pbogonko@safaricom.co.ke>
2023-08-15 17:36:42 +0300

    redid updates to the calculations

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -4,14 +4,20 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
 
     private  lateinit var weightValue : EditText
     private  lateinit var heightValue : EditText
     private  lateinit var bmiValue : TextView
+    private  lateinit var bmi : TextView
+    private  lateinit var submit : Button
+    private  lateinit var card : CardView
     private lateinit var  sharedPref: SharedPreferences
     private lateinit var  editor: SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,22 +26,31 @@ class MainActivity : AppCompatActivity() {
         weightValue = findViewById(R.id.etWeight)
         heightValue=findViewById(R.id.etHeight)
         bmiValue=findViewById(R.id.tvBMIValue)
-        var weight =25
-        var height =5
-        if(weightValue.text.toString().isNotEmpty() && heightValue.text.toString().isNotEmpty()) {
-            weight =weightValue.text.toString().toInt()
-            height =heightValue.text.toString().toInt()
-            height /= 100
-        }
-        Log.i("weight value is:","${weight}")
-        Log.i("height value is:","${height}")
+        bmi=findViewById(R.id.tvBmi)
+        submit=findViewById(R.id.btn)
+        card =findViewById(R.id.cardView2)
 
-        var bmiCalc=0;
-        if(weight>0 && height>0){
-            bmiCalc=weight/(height*height)
+
+        submit.setOnClickListener{
+
+            var weight =weightValue.text.toString().toInt()
+            var height =heightValue.text.toString().toInt()
+            height /= 100
+
+            Log.i("weight value is:","${weight}")
+            Log.i("height value is:","${height}")
+
+            var bmiCalc=0;
+            if(weight>0 && height>0){
+                bmiCalc=weight/(height*height)
+            }
+
+            card.visibility= View.VISIBLE
+            bmiValue.setText(bmiCalc.toString())
+            Log.i("bmi value is:","${bmiCalc}")
         }
-        bmiValue.setText(bmiCalc.toString())
-        Log.i("bmi value is:","${bmiCalc}")
+
+
         sharedPref=getSharedPreferences("my_sf", MODE_PRIVATE)
         editor=sharedPref.edit();
     }
